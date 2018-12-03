69281307ea3a4f2f88a1e5bd8516f49a71c63835
halim <halim.mekaki@gmail.com>
2018-12-03 12:59:02 +0100

    Ajout de la ligne de code permettant d'afficher les fichiers modifiés il y a moins de 7 jours

--- fiche_semaine_derniere.sh
+++ fiche_semaine_derniere.sh
@@ -4,6 +4,8 @@ echo "Welcome $USERNAME nous sommes le $today"
 read -p "Quel repertoire desirez-vous afficher ?" repertoire 
 cd $repertoire
 ls
+echo "Voici les fichiers modifiés il y a moins de 7 jours"
+find -type f -mtime -7
 
 
 
