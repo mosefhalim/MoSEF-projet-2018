16978213c9b94408afd7d198be89576e4ba4b8d0
halim <halim.mekaki@gmail.com>
2018-12-03 12:52:49 +0100

    Script permettant d'afficher le repertoire choisi

---
+++ fiche_semaine_derniere.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+today=$(date)
+echo "Welcome $USERNAME nous sommes le $today"
+read -p "Quel repertoire desirez-vous afficher ?" repertoire 
+cd $repertoire
+ls
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
